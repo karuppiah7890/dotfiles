e09d0a5a3145770b6f93b58db973446d141209da
Karuppiah Natarajan <karuppiah7890@gmail.com>
2022-12-14 11:32:16 +0530

    add master aliases list along with git aliases

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+# To source and manage all types of aliases in one place
+
+source git-aliases.sh
---
+++ git-aliases.sh
@@ -0,0 +1,13 @@
+# To source and manage all git related aliases in one place
+
+alias g="git"
+alias gst="git status"
+alias gco="git checkout"
+alias ga="git add"
+alias gc="git commit"
+alias gam="git commit --amend"
+alias gameo="git commit --amend --no-edit"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gp="git push -v"
+alias gup="git pull -v --rebase --autostash"
